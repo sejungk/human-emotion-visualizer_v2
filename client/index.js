7e52b4c14d3d075ec0c549dad353684530cdafff
Sejung <49921709+sejungk@users.noreply.github.com>
2023-03-21 01:05:05 -0400

    removed unused files

--- client/index.js
+++ client/index.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import store from './store'
-import App from './app'
-
-// establishes socket connection
-import './socket'
+import App from './app';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById('app')
 )
